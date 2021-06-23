017348a15426a49a2294e684bb5fdbc00b01e3a0
mengkang.zmk <mengkang.zmk@alibaba-inc.com>
2021-06-23 23:41:21 +0800

    命令行与监控联动

--- extension/deliverer.c
+++ extension/deliverer.c
@@ -28,53 +28,13 @@
 #include "php_deliverer.h"
 #include "main/SAPI.h"
 
-/* If you declare any globals in php_deliverer.h uncomment this:
-ZEND_DECLARE_MODULE_GLOBALS(deliverer)
-*/
 
-/* True global resources - no need for thread safety here */
 static int le_deliverer;
+
+static int deliverer_stat = -1;
 
 static FILE *fp;
 
-/* {{{ PHP_INI
- */
-/* Remove comments and fill if you need to have entries in php.ini
-PHP_INI_BEGIN()
-    STD_PHP_INI_ENTRY("deliverer.global_value",      "42", PHP_INI_ALL, OnUpdateLong, global_value, zend_deliverer_globals, deliverer_globals)
-    STD_PHP_INI_ENTRY("deliverer.global_string", "foobar", PHP_INI_ALL, OnUpdateString, global_string, zend_deliverer_globals, deliverer_globals)
-PHP_INI_END()
-*/
-/* }}} */
-
-
-PHP_FUNCTION(confirm_deliverer_compiled) /* {{{ */
-{
-	char *arg = NULL;
-	size_t arg_len, len;
-	zend_string *strg;
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS(), "s", &arg, &arg_len) == FAILURE) {
-		return;
-	}
-
-	strg = strpprintf(0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "deliverer", arg);
-
-	RETURN_STR(strg);
-}
-/* }}} */
-
-
-/* {{{ php_deliverer_init_globals
- */
-/* Uncomment this function if you have INI entries
-static void php_deliverer_init_globals(zend_deliverer_globals *deliverer_globals)
-{
-	deliverer_globals->global_value = 0;
-	deliverer_globals->global_string = NULL;
-}
-*/
-/* }}} */
 
 static char *get_function_name(zend_execute_data *call) /* {{{ */
 {
@@ -108,6 +68,10 @@ static char *get_function_name(zend_execute_data *call) /* {{{ */
 
 static int php_deliverer_log_handler(zend_execute_data *execute_data) /* {{{ */
 {
+    if (deliverer_stat == -1) {
+        return ZEND_USER_OPCODE_DISPATCH;
+    }
+
     zend_execute_data *call = execute_data->call;
     zend_function     *fbc  = call->func;
 
@@ -115,7 +79,6 @@ static int php_deliverer_log_handler(zend_execute_data *execute_data) /* {{{ */
         return ZEND_USER_OPCODE_DISPATCH;
     }
     char *method_or_function_name = get_function_name(call);
-    printf("%s\n", method_or_function_name);
 
     struct timeval tv;
     gettimeofday(&tv, NULL);
@@ -133,14 +96,16 @@ static int php_deliverer_log_handler(zend_execute_data *execute_data) /* {{{ */
 }
 /* }}} */
 
+static void set_deliverer_stat()  /* {{{ */
+{
+    deliverer_stat = access("/tmp/deliverer/config/stat", F_OK);
+}
+/* }}} */
 
 /* {{{ PHP_MINIT_FUNCTION
  */
 PHP_MINIT_FUNCTION(deliverer)
 {
-	/* If you have INI entries, uncomment these lines
-	REGISTER_INI_ENTRIES();
-	*/
     zend_set_user_opcode_handler(ZEND_DO_UCALL, php_deliverer_log_handler);
     zend_set_user_opcode_handler(ZEND_DO_FCALL_BY_NAME, php_deliverer_log_handler);
     zend_set_user_opcode_handler(ZEND_DO_FCALL, php_deliverer_log_handler);
@@ -153,14 +118,10 @@ PHP_MINIT_FUNCTION(deliverer)
  */
 PHP_MSHUTDOWN_FUNCTION(deliverer)
 {
-	/* uncomment this line if you have INI entries
-	UNREGISTER_INI_ENTRIES();
-	*/
 	return SUCCESS;
 }
 /* }}} */
 
-/* Remove if there's nothing to do at request start */
 /* {{{ PHP_RINIT_FUNCTION
  */
 PHP_RINIT_FUNCTION(deliverer)
@@ -169,13 +130,19 @@ PHP_RINIT_FUNCTION(deliverer)
 	ZEND_TSRMLS_CACHE_UPDATE();
 #endif
 
+    set_deliverer_stat();
+
+    if (deliverer_stat == -1) {
+        return SUCCESS;
+    }
+
 	struct timeval tv;
     gettimeofday(&tv, NULL);
 
 	long ts = tv.tv_sec * 1000000 + tv.tv_usec;
 
 	char str[128] = {0};
-    sprintf(str, "/tmp/deliverer/%d-%ld.log", getpid(), ts);
+    sprintf(str, "/tmp/deliverer/log/%d-%ld.log", getpid(), ts);
     fp = fopen(str, "a+");
     fprintf(fp, "---\n%d-%ld %s %s %s?%s\n",getpid() , tv.tv_sec * 1000000 + tv.tv_usec, sapi_module.name, SG(request_info).request_method, SG(request_info).request_uri, SG(request_info).query_string);
 
@@ -183,11 +150,14 @@ PHP_RINIT_FUNCTION(deliverer)
 }
 /* }}} */
 
-/* Remove if there's nothing to do at request end */
 /* {{{ PHP_RSHUTDOWN_FUNCTION
  */
 PHP_RSHUTDOWN_FUNCTION(deliverer)
 {
+    if (deliverer_stat == -1) {
+        return SUCCESS;
+    }
+
 	fprintf(fp, "---end---\n");
     fclose(fp);
 	return SUCCESS;
@@ -201,20 +171,14 @@ PHP_MINFO_FUNCTION(deliverer)
 	php_info_print_table_start();
 	php_info_print_table_header(2, "deliverer support", "enabled");
 	php_info_print_table_end();
-
-	/* Remove comments if you have entries in php.ini
-	DISPLAY_INI_ENTRIES();
-	*/
 }
 /* }}} */
 
 /* {{{ deliverer_functions[]
  *
- * Every user visible function must have an entry in deliverer_functions[].
  */
 const zend_function_entry deliverer_functions[] = {
-	PHP_FE(confirm_deliverer_compiled,	NULL)		/* For testing, remove later. */
-	PHP_FE_END	/* Must be the last line in deliverer_functions[] */
+	PHP_FE_END
 };
 /* }}} */
 
@@ -226,8 +190,8 @@ zend_module_entry deliverer_module_entry = {
 	deliverer_functions,
 	PHP_MINIT(deliverer),
 	PHP_MSHUTDOWN(deliverer),
-	PHP_RINIT(deliverer),		/* Replace with NULL if there's nothing to do at request start */
-	PHP_RSHUTDOWN(deliverer),	/* Replace with NULL if there's nothing to do at request end */
+	PHP_RINIT(deliverer),
+	PHP_RSHUTDOWN(deliverer),
 	PHP_MINFO(deliverer),
 	PHP_DELIVERER_VERSION,
 	STANDARD_MODULE_PROPERTIES

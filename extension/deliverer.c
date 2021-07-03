2ee3c80c62a5efea48d580b218c759ea3b0f63e7
mengkang.zmk <mengkang.zmk@alibaba-inc.com>
2021-07-03 15:08:15 +0800

    bugfix: null point

--- extension/deliverer.c
+++ extension/deliverer.c
@@ -40,7 +40,6 @@ static int deliverer_stat = -1;
 
 static FILE *fp;
 
-
 static char *get_function_name(zend_function *fbc) /* {{{ */
 {
     char *method_or_function_name = NULL;
@@ -48,40 +47,50 @@ static char *get_function_name(zend_function *fbc) /* {{{ */
 #if PHP_VERSION_ID < 70000
     const char *function_name = fbc->common.function_name;
 
-    if (function_name == NULL) return NULL;
+    if (function_name == NULL)
+    {
+        return NULL;
+    }
 
     zend_class_entry *scope = fbc->common.scope;
 
-    if (scope != NULL) {
+    if (scope != NULL)
+    {
         const char *class_name = scope->name;
-        int        len         = strlen(class_name) + strlen("::") + strlen(function_name) + 1;
-        method_or_function_name = (char *) emalloc(len);
+        int len = strlen(class_name) + strlen("::") + strlen(function_name) + 1;
+        method_or_function_name = (char *)emalloc(len);
+
         memset(method_or_function_name, 0, len);
         strcat(method_or_function_name, class_name);
         strcat(method_or_function_name, "::");
         strcat(method_or_function_name, function_name);
-    } else {
+    }
+    else
+    {
         int len = strlen(function_name) + 1;
-        method_or_function_name = (char *) emalloc(len);
+        method_or_function_name = (char *)emalloc(len);
         memset(method_or_function_name, 0, len);
         strcat(method_or_function_name, function_name);
     }
 #else
-    zend_string      *function_name = fbc->common.function_name;
-    zend_class_entry *scope         = fbc->common.scope;
-    char             *class_name    = NULL;
+    zend_string *function_name = fbc->common.function_name;
+    zend_class_entry *scope = fbc->common.scope;
+    char *class_name = NULL;
 
-    if (scope != NULL) {
+    if (scope != NULL)
+    {
         class_name = ZSTR_VAL(scope->name);
-        int len    = strlen(class_name) + strlen("::") + ZSTR_LEN(function_name) + 1;
-        method_or_function_name = (char *) emalloc(len);
+        int len = strlen(class_name) + strlen("::") + ZSTR_LEN(function_name) + 1;
+        method_or_function_name = (char *)emalloc(len);
         memset(method_or_function_name, 0, len);
         strcat(method_or_function_name, class_name);
         strcat(method_or_function_name, "::");
         strcat(method_or_function_name, ZSTR_VAL(function_name));
-    } else {
+    }
+    else
+    {
         int len = ZSTR_LEN(function_name) + 1;
-        method_or_function_name = (char *) emalloc(len);
+        method_or_function_name = (char *)emalloc(len);
         memset(method_or_function_name, 0, len);
         strcat(method_or_function_name, ZSTR_VAL(function_name));
     }
@@ -97,10 +106,14 @@ static zend_function *get_function_from_opline(zend_op *opline) /* {{{ */
 
     zval *function_name = OP1_FUNCTION_PTR(opline);
 
-    if (Z_STRVAL_P(function_name) == NULL) return NULL;
+    if (Z_STRVAL_P(function_name) == NULL)
+    {
+        return NULL;
+    }
 
-    if (zend_hash_find(EG(function_table), Z_STRVAL_P(function_name), Z_STRLEN_P(function_name) + 1, (void **) &fbc) ==
-        FAILURE) {
+    if (zend_hash_find(EG(function_table), Z_STRVAL_P(function_name), Z_STRLEN_P(function_name) + 1, (void **)&fbc) ==
+        FAILURE)
+    {
         return NULL;
     }
 
@@ -110,29 +123,43 @@ static zend_function *get_function_from_opline(zend_op *opline) /* {{{ */
 
 static int php_deliverer_log_handler(zend_execute_data *execute_data) /* {{{ */
 {
-    if (deliverer_stat == -1) {
+    if (deliverer_stat == -1)
+    {
         return ZEND_USER_OPCODE_DISPATCH;
     }
 
     zend_function *fbc;
 
 #if PHP_VERSION_ID < 70000
-    #if PHP_VERSION_ID < 50500
-    if (execute_data->fbc != NULL) fbc = execute_data->fbc; // function inner call
-    #else
-    if (execute_data->call != NULL) fbc = execute_data->call->fbc;// function inner call
-    #endif
-
-    if (fbc == NULL) {
+#if PHP_VERSION_ID < 50500
+    if (execute_data->fbc != NULL)
+    {
+        fbc = execute_data->fbc; // function inner call
+    }
+#else
+    if (execute_data->call != NULL && execute_data->call->fbc != NULL)
+    {
+        fbc = execute_data->call->fbc; // function inner call
+    }
+#endif
+    if (fbc == NULL)
+    {
         fbc = get_function_from_opline(execute_data->opline);
     }
 #else
-    fbc = execute_data->call->func;
+    if (execute_data->call != NULL && execute_data->call->func != NULL)
+    {
+        fbc = execute_data->call->fbc;
+    }
 #endif
 
-    if (fbc == NULL) return ZEND_USER_OPCODE_DISPATCH;
+    if (fbc == NULL)
+    {
+        return ZEND_USER_OPCODE_DISPATCH;
+    }
 
-    if (fbc->type != ZEND_USER_FUNCTION) {
+    if (fbc->type != ZEND_USER_FUNCTION)
+    {
         return ZEND_USER_OPCODE_DISPATCH;
     }
 
@@ -141,7 +168,7 @@ static int php_deliverer_log_handler(zend_execute_data *execute_data) /* {{{ */
     struct timeval tv;
     gettimeofday(&tv, NULL);
     const char *filename = zend_get_executed_filename();
-    int        lineno    = zend_get_executed_lineno();
+    int lineno = zend_get_executed_lineno();
 
     //format time|current_p|prev_p|function|filename|line
     fprintf(fp, "%ld|%p|%p|%s|%s|%d\n", tv.tv_sec * 1000000 + tv.tv_usec, execute_data, execute_data->prev_execute_data,
@@ -153,32 +180,35 @@ static int php_deliverer_log_handler(zend_execute_data *execute_data) /* {{{ */
 }
 /* }}} */
 
-static void set_deliverer_stat()  /* {{{ */
+static void set_deliverer_stat() /* {{{ */
 {
     deliverer_stat = access("/tmp/deliverer/config/stat", F_OK);
 }
 /* }}} */
 
-static char *build_deliverer_cli_argv()  /* {{{ */
+static char *build_deliverer_cli_argv() /* {{{ */
 {
     char *cli_argv_string;
 
     int len = 0;
     int i;
 
-    if (SG(request_info).argc > 0) {
+    if (SG(request_info).argc > 0)
+    {
 
-        for (i = 0; i < SG(request_info).argc; i++) {
+        for (i = 0; i < SG(request_info).argc; i++)
+        {
             len += strlen(SG(request_info).argv[i]);
         }
 
         len += SG(request_info).argc;
 
-        cli_argv_string = (char *) emalloc(len + SG(request_info).argc);
+        cli_argv_string = (char *)emalloc(len + SG(request_info).argc);
 
         memset(cli_argv_string, 0, len);
 
-        for (i = 0; i < SG(request_info).argc; i++) {
+        for (i = 0; i < SG(request_info).argc; i++)
+        {
             strcat(cli_argv_string, SG(request_info).argv[i]);
             strcat(cli_argv_string, " ");
         }
@@ -200,16 +230,15 @@ PHP_MINIT_FUNCTION(deliverer)
 
     zend_set_user_opcode_handler(ZEND_DO_FCALL_BY_NAME, php_deliverer_log_handler);
     zend_set_user_opcode_handler(ZEND_DO_FCALL, php_deliverer_log_handler);
-	return SUCCESS;
+    return SUCCESS;
 }
 /* }}} */
-
 
 /* {{{ PHP_MSHUTDOWN_FUNCTION
  */
 PHP_MSHUTDOWN_FUNCTION(deliverer)
 {
-	return SUCCESS;
+    return SUCCESS;
 }
 /* }}} */
 
@@ -223,7 +252,8 @@ PHP_RINIT_FUNCTION(deliverer)
 
     set_deliverer_stat();
 
-    if (deliverer_stat == -1) {
+    if (deliverer_stat == -1)
+    {
         return SUCCESS;
     }
 
@@ -236,7 +266,8 @@ PHP_RINIT_FUNCTION(deliverer)
     sprintf(str, "/tmp/deliverer/log/%d-%ld.log", getpid(), ts);
     fp = fopen(str, "a+");
 
-    if (strcmp(sapi_module.name, "cli") == 0) {
+    if (strcmp(sapi_module.name, "cli") == 0)
+    {
 
         char *cli_argv_string = build_deliverer_cli_argv();
 
@@ -244,8 +275,9 @@ PHP_RINIT_FUNCTION(deliverer)
                 cli_argv_string);
 
         efree(cli_argv_string);
-
-    } else {
+    }
+    else
+    {
         fprintf(fp, "---\n%d-%ld %s %s %s %s\n", getpid(), tv.tv_sec * 1000000 + tv.tv_usec, sapi_module.name,
                 SG(request_info).request_method, SG(request_info).request_uri, SG(request_info).query_string);
     }
@@ -258,13 +290,14 @@ PHP_RINIT_FUNCTION(deliverer)
  */
 PHP_RSHUTDOWN_FUNCTION(deliverer)
 {
-    if (deliverer_stat == -1) {
+    if (deliverer_stat == -1)
+    {
         return SUCCESS;
     }
 
-	fprintf(fp, "---end---");
+    fprintf(fp, "---end---");
     fclose(fp);
-	return SUCCESS;
+    return SUCCESS;
 }
 /* }}} */
 
@@ -272,9 +305,9 @@ PHP_RSHUTDOWN_FUNCTION(deliverer)
  */
 PHP_MINFO_FUNCTION(deliverer)
 {
-	php_info_print_table_start();
-	php_info_print_table_header(2, "deliverer support", "enabled");
-	php_info_print_table_end();
+    php_info_print_table_start();
+    php_info_print_table_header(2, "deliverer support", "enabled");
+    php_info_print_table_end();
 }
 /* }}} */
 
